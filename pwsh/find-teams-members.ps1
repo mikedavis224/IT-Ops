a5c76668e9e37145b7465d52b928b9d2e7ec31ea
Mike Davis <mdavis@delcor.com>
2025-02-18 18:19:11 -0500

    added local scripts

---
+++ pwsh/find-teams-members.ps1
@@ -0,0 +1,3 @@
+﻿41c46d92-4560-49de-8fe8-48e80cd3c063
+Get-TeamUser -GroupId 41c46d92-4560-49de-8fe8-48e80cd3c063 | Select-Object UserId, User, Role | Export-Csv -Path "C:\Export\2025-Participant-NetworkingMembers.csv" -NoTypeInformation
+Get-TeamUser -GroupId 41c46d92-4560-49de-8fe8-48e80cd3c063 | Select-Object @{Name="DisplayName";Expression={$_.'User'}}, @{Name="Email";Expression={$_.'UserId'}}, Role | Export-Csv -Path "C:\Export\2025-Participant-Networking-Members.csv" -NoTypeInformation
